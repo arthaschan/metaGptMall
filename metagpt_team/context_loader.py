cf4468fb6cd59bcf402ca8d17876facd677630e3
陈天元 <arthaschan@163.com>
2026-04-10 02:17:13 +0800

    feat: add MetaGPT SDK multi-role team runner + committed latest snapshot

---
+++ metagpt_team/context_loader.py
@@ -0,0 +1,10 @@
+# Context Loader
+
+# This module is responsible for loading the context for the MetaGPT team.
+
+class ContextLoader:
+    def __init__(self):
+        pass
+
+    def load_context(self):
+        pass
